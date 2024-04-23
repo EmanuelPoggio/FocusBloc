f2ce38479484005ea339250966ccdc16f307adf3
Emanuel_Poggio <epoggio@atlan-tics.com>
2024-04-22 23:44:23 -0300

    Agregando estructura de carpetas y menú principal

---
+++ scripts/popup.js
@@ -0,0 +1,8 @@
+// popup.js
+document.getElementById('open-pomodoro').addEventListener('click', function() {
+    window.location.href = '../pages/pomodoro.html'; 
+});
+
+document.getElementById('open-site-timer').addEventListener('click', function() {
+    window.location.href = '../pages/site-timer.html'; 
+});
